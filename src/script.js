e935fc6d289f3843afc50f5a9c19e3150650e8b9
Masibanda <manatsanadia@gmail.com>
2023-09-21 20:04:57 +0400

    weather application
    initial commit

---
+++ src/script.js
@@ -0,0 +1,87 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+let dateElement = document.querySelector("#current-date-and-time");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#feel").innerHTML = response.data.main.feels_like;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+function searchCity(event) {
+  let apiKey = "f746983cf7bc5c09c8d7bff7c5218dde";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function searchBtnClick(click) {
+  invokeSearchFunction();
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  invokeSearchFunction();
+}
+
+function invokeSearchFunction() {
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation() {
+  navigator.geolocation.getCurrentPosition(getCurrentLocation);
+}
+
+function getCurrentLocation(position) {
+  let apiKey = "f746983cf7bc5c09c8d7bff7c5218dde";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#location");
+currentLocationButton.addEventListener("click", searchLocation);
