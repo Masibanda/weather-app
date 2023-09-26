51badf7a47e9d48247325eb10f5f113d39bbbad4
Masibanda <manatsanadia@gmail.com>
2023-09-26 04:17:02 +0400

    weather forecast API integration

--- src/script.js
+++ src/script.js
@@ -1,35 +1,14 @@
-// function formatDate(timestamp) {
-//   let date = new Date(timestamp);
-//   let hours = date.getHours();
-//   if (hours < 10) {
-//     hours = `0${hours}`;
-//   }
-//   let minutes = date.getMinutes();
-//   if (minutes < 10) {
-//     minutes = `0${minutes}`;
-//   }
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-//   let days = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//   ];
-//   let day = days[date.getDay()];
-//   return `${day} ${hours}:${minutes}`;
-// }
-
-// Format date and timezone
-function formatDate(date, timezone) {
-  let localOffsetInMs = date.getTimezoneOffset() * 60 * 1000;
-  let targetOffsetInMs = timezone * 1000;
-  let targetTimestamp = date.getTime() + localOffsetInMs + targetOffsetInMs;
-
-  let now = new Date(targetTimestamp);
-
+  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -39,44 +18,69 @@ function formatDate(date, timezone) {
     "Friday",
     "Saturday"
   ];
-  let day = days[now.getDay()];
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "October",
-    "November",
-    "December"
-  ];
-  let month = months[now.getMonth()];
-  let dayIndex = now.getDate();
-  let year = now.getFullYear();
+  let day = days[dayIndex];
 
-  let hours = now.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  let currentTime = `${hours}:${minutes}`;
-  let dateTimeElement = document.querySelector("#current-date-and-time");
-  dateTimeElement.innerHTML = `${day}, ${dayIndex} ${month} ${year} &nbsp;|&nbsp; Local time: ${currentTime}`;
-
-  let formattedDate = `${day}, ${dayIndex} ${month} ${year} &nbsp;|&nbsp; Local time: ${currentTime}`;
-
-  return formattedDate;
+  return `${day} ${hours}:${minutes}`;
 }
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "f746983cf7bc5c09c8d7bff7c5218dde";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
 function displayWeatherCondition(response) {
-  // let dateElement = document.querySelector("#date");
-  // dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
@@ -93,8 +97,9 @@ function displayWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  // document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
-}
+  getForecast(response.data.coord);
+  }
+
 function searchCity(event) {
   let apiKey = "f746983cf7bc5c09c8d7bff7c5218dde";
   let city = document.querySelector("#city-input").value;
@@ -125,20 +130,9 @@ function getCurrentLocation(position) {
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
-}
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
-}
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#location");
 currentLocationButton.addEventListener("click", searchLocation);
+searchCity("London");
